1a5e41d37780526a6dd725dbc07fdd721e118a85
igor-correia <igorcorreia425@gmail.com>
2024-10-02 23:26:53 -0300

    utilizando escrita exclusiva e com prioridade

--- ECommerce/src/main/java/com/ecommerce/Estoque.java
+++ ECommerce/src/main/java/com/ecommerce/Estoque.java
@@ -2,32 +2,87 @@ package main.java.com.ecommerce;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Semaphore;
 
 class Estoque {
     private final Map<String, Produto> produtos;
+    private final Semaphore mutex = new Semaphore(1);
+    private final Semaphore catracaEscrita = new Semaphore(1);
+    private final Semaphore catracaLeitura = new Semaphore(1);
+    private int countLeitor = 0;
+    private int countEscritor = 0;
 //    Preciso de um lock aqui
 
     public Estoque() {
         produtos = new ConcurrentHashMap<>();
     }
 
-    public void adicionarProduto(Produto produto) {
+    public void adicionarProduto(Produto produto) throws InterruptedException {
+        mutex.acquire();
+        countEscritor++;
+        if (countEscritor == 1) {
+            catracaLeitura.acquire();
+        }
+        mutex.release();
+
+        catracaEscrita.acquire();
+
         produtos.put(produto.getNome(), produto);
+
+        catracaEscrita.release();
+
+        mutex.acquire();
+        countEscritor--;
+        if (countEscritor == 0) {
+            catracaLeitura.release();
+        }
+        mutex.release();
        
     }
 
-    public boolean verificarDisponibilidade(Pedido pedido) {
-    	for (Map.Entry<Produto, Integer> entry : pedido.getItens()) {
+    public boolean verificarDisponibilidade(Pedido pedido) throws InterruptedException {
+    	catracaLeitura.acquire();
+        catracaLeitura.release();
+
+        mutex.acquire();
+        countLeitor++;
+        if (countLeitor == 1) {
+            catracaEscrita.acquire();
+        }
+        mutex.release();
+
+        boolean data = false;
+        for (Map.Entry<Produto, Integer> entry : pedido.getItens()) {
             Produto p = entry.getKey();
             int quantidadePedido = entry.getValue();
             if (!produtos.containsKey(p.getNome()) || produtos.get(p.getNome()).getQuantidade() < quantidadePedido) {
-                return false;
+                data = false;
+                break;
             }
+            data = true;
         }
-        return true;    
+          
+
+        mutex.acquire();
+        countLeitor--;
+        if (countLeitor == 0) {
+            catracaEscrita.release(); 
+        }
+        mutex.release();
+
+        return data;
     }
 
-    public void atualizarEstoque(Pedido pedido) {
+    public void atualizarEstoque(Pedido pedido)  throws InterruptedException {
+        mutex.acquire();
+        countEscritor++;
+        if (countEscritor == 1) {
+            catracaLeitura.acquire();
+        }
+        mutex.release();
+
+        catracaEscrita.acquire();
+
     	for (Map.Entry<Produto, Integer> entry : pedido.getItens()) {
             Produto p = entry.getKey();
             int quantidadePedido = entry.getValue();
@@ -36,5 +91,13 @@ class Estoque {
                 produtoEstoque.retirarProduto(quantidadePedido);
             }
         }
+        catracaEscrita.release();
+
+        mutex.acquire();
+        countEscritor--;
+        if (countEscritor == 0) {
+            catracaLeitura.release();
+        }
+        mutex.release();
     }
 }
